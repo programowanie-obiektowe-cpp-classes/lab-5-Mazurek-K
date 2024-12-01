725473118c605b8d9ac191f4dd6d29014d595ce1
Mazurek-K <mazurekk21@gmail.com>
2024-12-01 11:06:11 +0100

    works

--- foo.hpp
+++ foo.hpp
@@ -7,6 +7,26 @@
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    // Twoja implementacja tutaj
-    return {};
+    std::vector< char > ret;
+
+     for (auto it = people.begin(); it != people.end(); ++it) {
+        it->birthday(); 
+     }
+
+     
+     for (auto it = people.rbegin(); it != people.rend(); ++it) {
+         if (it->isMonster() == true) {
+             ret.push_back('n');
+         }
+         else {
+             ret.push_back('y');
+         }
+     }
+
+  
+
+    return ret;
+
+
+
 }
